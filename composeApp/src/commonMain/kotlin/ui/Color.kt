ff27577a3d4557b51624a238cc39cd29fe9afc64
milanmijic <milan.mijic@vegait.rs>
2024-05-24 10:57:10 +0200

    Code cleaned up

--- composeApp/src/commonMain/kotlin/ui/Color.kt
+++ composeApp/src/commonMain/kotlin/ui/Color.kt
@@ -2,18 +2,8 @@ package ui
 
 import androidx.compose.ui.graphics.Color
 
-val DarkGray = Color(0xFF202020)
-val MediumGray = Color(0xFF505050)
-val ColorPrimary = Color(0xFFFF5722)
-val TextWhite = Color(0xFFEEEEEE)
-
 val TextGray = Color(0xFFAEB6CE)
-val BlueDark = Color(0xFF4A7DDF)
-val BlueLight = Color(0xFF4AD6DF)
 val BackgroundGray = Color(0xCCE8E8E8)
-val TextDarkGray = Color(0xFF8D8B8B)
-val ColorOrange = Color(0xFFEE6855)
-val ColorOrangeLight = Color(0xFFFFA79B)
 
 val ColorPurple = Color(0xFF7D5DB0)
 val ColorYellow = Color(0xFFFDE400)
