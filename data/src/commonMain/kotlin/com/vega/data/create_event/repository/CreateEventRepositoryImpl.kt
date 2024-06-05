61f0a8399125587ce3afb423a1c711e7a6fb6cc6
milanmijic <milan.mijic@vegait.rs>
2024-06-05 12:03:40 +0200

    Code cleaned up

--- data/src/commonMain/kotlin/com/vega/data/create_event/repository/CreateEventRepositoryImpl.kt
+++ data/src/commonMain/kotlin/com/vega/data/create_event/repository/CreateEventRepositoryImpl.kt
@@ -2,11 +2,8 @@ package com.vega.data.create_event.repository
 
 import com.vega.data.auth.storage.SettingsStorage
 import com.vega.data.create_event.remote.CreateEventApi
-import com.vega.data.update_event.remote.UpdateEventApi
 import com.vega.domain.model.create_event.CreateEventRequestBody
-import com.vega.domain.model.update_event.UpdateEventRequestBody
 import com.vega.domain.repository.create_event.CreateEventRepository
-import com.vega.domain.repository.update_event.UpdateEventRepository
 
 class CreateEventRepositoryImpl(
     private val api: CreateEventApi,
